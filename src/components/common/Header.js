2288ef45bfedb32bc968d57e24ab61549a22aeb8
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-07 16:46:54 +0200

    Clean up the header.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,7 +3,6 @@
  */
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
-import propTypes from 'prop-types'
 
 import PrivateRoute from '../../containers/PrivateRoute'
 import LoginDialog from '../../containers/LoginDialog'
@@ -11,16 +10,6 @@ import HomePage from '../pages/HomePage'
 import LotteryPage from '../pages/LotteryPage'
 
 class Header extends Component {
-
-  showLoginDialog() {
-    this.props.showLogin()
-  }
-
-  closeLoginDialog(e) {
-    if (e.target.className.includes('Dialog DialogModal') || e.key === 'Escape') {
-      this.props.hideLogin()
-    }
-  }
 
   render() {
     return (
@@ -34,20 +23,9 @@ class Header extends Component {
         <Route exact path='/' component={HomePage}/>
         <Route path='/login' component={LoginDialog}/>
         <PrivateRoute path='/lotteries' component={LotteryPage}/>
-
-        {this.props.showLoginModal && !this.props.isAuthenticated ?
-          <LoginDialog closeDialog={this.closeLoginDialog.bind(this)}/> : null}
       </div>
     )
   }
 }
-
-Header.propTypes = {
-  showLoginModal: propTypes.bool,
-  isAuthenticated: propTypes.bool,
-  showLogin: propTypes.func,
-  hideLogin: propTypes.func
-}
-
 
 export default Header
