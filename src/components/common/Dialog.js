362b08bc86b8a590f9d4d0db61cc271b47589c5d
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-12 13:30:21 +0200

    Cleaned the Dialog component.

--- src/components/common/Dialog.js
+++ src/components/common/Dialog.js
@@ -8,21 +8,10 @@ import React, { Component } from 'react'
 import propTypes from 'prop-types'
 
 class Dialog extends Component {
-
-  componentWillUnmount() {
-    document.body.classList.remove('DialogModalOpen')
-  }
-
-  componentDidMount() {
-    if (this.props.modal) {
-      document.body.classList.add('DialogModalOpen')
-    }
-  }
-
   render() {
     return (
       <StyledDialog>
-        <div className={this.props.modal ? 'DialogModal__wrap' : null}>
+        <div>
           <div className='Dialog__header'>{this.props.header}</div>
           <div className='Dialog__body'>{this.props.children}</div>
           <div className='Dialog__footer'>
@@ -41,7 +30,6 @@ class Dialog extends Component {
 Dialog.propTypes = {
   header: propTypes.string.isRequired,
   confirmLabel: propTypes.string,
-  modal: propTypes.bool,
   onAction: propTypes.func,
   type: propTypes.string,
   className: propTypes.string
@@ -49,7 +37,6 @@ Dialog.propTypes = {
 
 Dialog.defaultProps = {
   confirmLabel: 'ok',
-  modal: false,
   onAction: () => {
   }
 }
