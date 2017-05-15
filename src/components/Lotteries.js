6da68cd7fd6b99570fdf622aa81ad58db0619a43
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-15 19:16:09 +0200

    Getting the lotteries data from the API.

--- src/components/Lotteries.js
+++ src/components/Lotteries.js
@@ -10,22 +10,23 @@ import Lottery from './Lottery'
 export default class Lotteries extends Component {
 
   render() {
-
-
-    this.props.lotteriesData.map((el) => {
-      console.log(el)
-    })
-
     const lotteries = []
 
     if (this.props.lotteriesData) {
-      this.props.lotteriesData.forEach((entry) => {
-        lotteries.push(<Lottery name={entry.id}
-                                jackpot={entry.jackpots[0].jackpot}
-                                drawingDate={entry.drawingDate}
-                                key={entry.id} />)
+      this.props.lotteriesData.map((lottery) => {
+        lotteries.push(<Lottery name={lottery.getIn(['id'], '')}
+                                jackpot={lottery.getIn(['jackpots'], [])[0]}
+                                drawingDate={lottery.getIn(['drawingDate'], '')}
+                                key={Math.random()} />)
       })
     }
+
+  /*this.props.lotteriesData.forEach((entry) => {
+    lotteries.push(<Lottery name={entry.id}
+                            jackpot={entry.jackpots[0].jackpot}
+                            drawingDate={entry.drawingDate}
+                            key={entry.id} />)
+  })*/
 
 
     return (
