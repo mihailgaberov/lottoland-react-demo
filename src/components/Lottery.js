4b9ad57804fc8a434f61e7bee93fff58e8b550d9
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-16 12:05:10 +0200

    Added lotteries logos.

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -6,8 +6,12 @@ import Jackpots from './Jackpots'
 
 export default class Lottery extends Component {
   render() {
+
+    const imgPath = require(`${'../../logos/' + this.props.name}.png`)
+
     return (
       <ul>
+        <li><img src={imgPath} alt={this.props.name} /></li>
         <li>Name: {this.props.name}</li>
         <li>Jackpots: <Jackpots data={this.props.jackpots} /></li>
         <li>Drawing date: {this.props.drawingDate}</li>
