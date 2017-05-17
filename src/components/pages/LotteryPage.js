c94bef801facec44edeca8ca03bff5d7e38ec9ee
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-17 11:58:43 +0200

    Removed inline styles for the Wrapper component.

--- src/components/pages/LotteryPage.js
+++ src/components/pages/LotteryPage.js
@@ -2,22 +2,14 @@
  * Created by mgab on 04/05/2017.
  */
 import React, { Component } from 'react'
-import styled from 'styled-components'
 import { connect } from 'react-redux'
 
+import Wrapper from '../common/styled-components/Wrapper'
 import Button from '../common/styled-components/Button'
 import { logOut } from '../../actions/authActions'
 import { getLotteriesData, getData } from '../../actions/lotteriesDataActions'
 import Lotteries from '../Lotteries'
 
-const Wrapper = styled.section`
-  padding: 2em;
-  background: papayawhip;
-  
-  @media only screen and (min-width: 680px) {
-    padding: 4em;
-  }
-`
 class LotteryPage extends Component {
 
   constructor() {
@@ -53,41 +45,6 @@ class LotteryPage extends Component {
 const mapStateToProps = (state) => {
   return {
     lotteries: getLotteriesData(state)
-    // lotteries : [
-    //   {
-    //     "id": "austriaLotto_2627",
-    //     "lotteryId": "austriaLotto",
-    //     "drawingDate": "2017-05-14T16:30:00.000+0000",
-    //     "closingDate": "2017-05-14T16:00:00.000+0000",
-    //     "state": "IN_PLAY",
-    //     "doubleJackpotAllowed": true,
-    //     "jackpots": [
-    //       {
-    //         "lotteryId": "austriaLotto",
-    //         "jackpot": 10000,
-    //         "marketingJackpot": 0
-    //       }
-    //     ],
-    //     "drawingType": "SU"
-    //   },
-    //
-    //   {
-    //     "id": "cash4Life_305",
-    //     "lotteryId": "cash4Life",
-    //     "drawingDate": "2017-05-16T01:00:00.000+0000",
-    //     "closingDate": "2017-05-16T00:00:00.000+0000",
-    //     "state": "IN_PLAY",
-    //     "doubleJackpotAllowed": true,
-    //     "jackpots": [
-    //       {
-    //         "lotteryId": "cash4Life",
-    //         "jackpot": 222220,
-    //         "marketingJackpot": 0
-    //       }
-    //     ],
-    //     "drawingType": "TU"
-    //   }
-    // ]
   }
 }
 
