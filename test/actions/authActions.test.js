4272f7269b62812b97958c85772a75a31ef04010
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-17 16:06:08 +0200

    Arrange the tests into directories. Adding tests for the auth actions.

--- test/actions/authActions.test.js
+++ test/actions/authActions.test.js
@@ -3,6 +3,7 @@
  */
 import * as actions from '../../src/actions/authActions'
 import * as types from '../../src/constants/actionTypes'
+import { Map, fromJS } from 'immutable'
 
 describe('Auth Actions', () => {
   it('should create an action to log in the user', () => {
@@ -21,5 +22,20 @@ describe('Auth Actions', () => {
       type: types.AUTH_LOGOUT
     }
     expect(actions.logOut()).toEqual(expectedAction)
+  })
+
+  it('should provide method for checking if errors', () => {
+    const state = Map()
+    expect(actions.isError(state)).toEqual(false)
+    const resp = {
+      'auth': {
+        'authInfo': {
+          "error": "invalid_grant",
+          "error_description": "LOGIN_FAILED"
+        }
+      }
+    }
+    const state1 = state.mergeDeep(fromJS(resp))
+    expect(actions.isError(state1)).toEqual(true)
   })
 })
