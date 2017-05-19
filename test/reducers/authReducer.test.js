8d6e064593fc7241dd7b79f82f6f485769cdabc1
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-19 13:43:33 +0200

    Added tests for authReducer.

--- test/reducers/authReducer.test.js
+++ test/reducers/authReducer.test.js
@@ -7,23 +7,39 @@ import { Map, fromJS } from 'immutable'
 
 describe('Authentication reducer', () => {
   const initialState = {
-    payload: [{
+    payload: {
       username: 'mihail.gaberov@lottoland.com',
       password: 'testtest'
-    }]
+    }
   }
 
   it('should return the initial state', () => {
-    expect(reducer(undefined, [])).toEqual(fromJS({}));
+    expect(reducer(undefined, [])).toEqual(fromJS({}))
   })
 
   it('should return successful login if credentials are correct', () => {
-    const testAction = { type: types.AUTH_RESPONSE, payload: {'access_token': 'asdsada'} };
-    expect(reducer(initialState, testAction)).toEqual(fromJS({"payload": {"access_token": "asdsada"}, "type": "AUTH_RESPONSE"}));
+    const testAction = { type: types.AUTH_RESPONSE, payload: { 'access_token': 'asdsada' } }
+    expect(reducer(initialState, testAction)).toEqual(fromJS({
+      "payload": { "access_token": "asdsada" },
+      "type": "AUTH_RESPONSE"
+    }))
   })
 
   it('should handle logout', () => {
-    const testAction = { type: types.AUTH_LOGOUT };
-    expect(reducer(initialState, testAction)).toEqual(Map());
+    const testAction = { type: types.AUTH_LOGOUT }
+    expect(reducer(initialState, testAction)).toEqual(Map())
+  })
+
+  it('should handle the case when an error occurred', () => {
+    const testAction = { type: types.AUTH_ERROR }
+
+    let state = Map()
+    state = state.set('payload', {
+      username: 'mihail.gaberov@lottoland.com',
+        password: 'testtest'
+    })
+    state = state.set('error', undefined)
+
+    expect(reducer(initialState, testAction)).toEqual(state)
   })
 })
