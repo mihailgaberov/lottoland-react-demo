54d628e1f9177764550c2f8f5c24d3020a5fb14b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-06-03 14:22:24 +0200

    Removed redundant semicolons.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,10 +2,10 @@
  * Created by mgab on 05/05/2017.
  */
 
-import { fork } from 'redux-saga/effects';
-import watchAuthentication, { watchLotteriesData } from './watcher';
+import { fork } from 'redux-saga/effects'
+import watchAuthentication, { watchLotteriesData } from './watcher'
 
 export default function* startForman() {
-  yield fork(watchAuthentication);
-  yield fork(watchLotteriesData);
+  yield fork(watchAuthentication)
+  yield fork(watchLotteriesData)
 }
