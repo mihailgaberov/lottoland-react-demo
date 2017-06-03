ff0037a0b5459e4de0a7c87c54e1a65a3d397b4a
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-06-03 14:15:00 +0200

    Removed redundant semicolons.

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,15 +1,15 @@
 /**
  * Created by mgab on 05/05/2017.
  */
-import { takeLatest } from 'redux-saga/effects';
-import { authSaga } from './authSaga';
-import { lotteriesSaga } from './lotteriesSaga';
-import * as types from '../constants/actionTypes';
+import { takeLatest } from 'redux-saga/effects'
+import { authSaga } from './authSaga'
+import { lotteriesSaga } from './lotteriesSaga'
+import * as types from '../constants/actionTypes'
 
 export default function* watchAuthentication() {
-  yield takeLatest(types.AUTH_REQUEST, authSaga);
+  yield takeLatest(types.AUTH_REQUEST, authSaga)
 }
 
 export function* watchLotteriesData() {
-  yield takeLatest(types.DATA_REQUEST, lotteriesSaga);
+  yield takeLatest(types.DATA_REQUEST, lotteriesSaga)
 }
